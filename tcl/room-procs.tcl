29c4cd4abb0a2651fd030a6655a28018af6352cd
Antonio Pisano <antonio@elettrotecnica.it>
2022-10-30 22:37:44 +0100

    Fix the chance for race conditions when creating the room

--- tcl/room-procs.tcl
+++ tcl/room-procs.tcl
@@ -16,36 +16,61 @@ ad_proc -private aframe_vr::room::require {
     if {![info exists package_id]} {
         set package_id [ad_conn package_id]
     }
-    if {![nsv_array exists aframe-vr-janus-rooms] ||
-        ![nsv_get aframe-vr-janus-rooms $package_id janus_room]} {
-        set janus_room [parameter::get -package_id $package_id -parameter janus_room -default $package_id]
+    #
+    # Check if room exists in the nsd
+    #
+    if {![nsv_get aframe-vr-janus-rooms $package_id janus_room]} {
+        #
+        # We have to create the room. Lock this operations so that
+        # other threads don't get in the way.
+        #
+        ns_mutex eval [nsv_get aframe-vr-janus-rooms mutex] {
+            #
+            # Before we start the room creation, try again the nsv in
+            # case we are one of those threads that were blocked
+            # before.
+            #
+            if {[nsv_get aframe-vr-janus-rooms $package_id janus_room]} {
+                return $janus_room
+            }
 
-        set session_url [::janus::create_session -package_id $package_id]
-        set plugin_url [::janus::plugin::attach \
-                            -session_url $session_url \
-                            -plugin janus.plugin.videoroom]
-        set room_exists_p [::janus::videoroom::exists_p \
-                               -plugin_url $plugin_url \
-                               -room $janus_room]
-        if {!$room_exists_p} {
-            set janus_room_pin [expr {rand() * 10000}]
-            set janus_room [::janus::videoroom::create \
-                                -plugin_url $plugin_url \
-                                -room $janus_room \
-                                -description "AFrame VR room for package $package_id" \
-                                -pin $janus_room_pin \
-                                -publishers $publishers]
-            ns_log warning $room
-            parameter::set_value \
-                -package_id $package_id \
-                -parameter janus_room \
-                -value $janus_room
-            parameter::set_value \
-                -package_id $package_id \
-                -parameter janus_room_pin \
-                -value $janus_room_pin
+            #
+            # If we are here we were the first to try and create this
+            # room. Proceed.
+            #
+            set janus_room [parameter::get -package_id $package_id -parameter janus_room -default $package_id]
+
+            set session_url [::janus::create_session -package_id $package_id]
+            set plugin_url [::janus::plugin::attach \
+                                -session_url $session_url \
+                                -plugin janus.plugin.videoroom]
+
+            set room_exists_p [::janus::videoroom::exists_p \
+                                   -plugin_url $plugin_url \
+                                   -room $janus_room]
+            if {!$room_exists_p} {
+                set janus_room_pin [expr {rand() * 10000}]
+                set janus_room [::janus::videoroom::create \
+                                    -plugin_url $plugin_url \
+                                    -room $janus_room \
+                                    -description "AFrame VR room for package $package_id" \
+                                    -pin $janus_room_pin \
+                                    -publishers $publishers]
+                parameter::set_value \
+                    -package_id $package_id \
+                    -parameter janus_room \
+                    -value $janus_room
+                parameter::set_value \
+                    -package_id $package_id \
+                    -parameter janus_room_pin \
+                    -value $janus_room_pin
+            }
+
+            #
+            # Success! Save the room in the nsv.
+            #
+            nsv_set aframe-vr-janus-rooms $package_id $janus_room
         }
-        nsv_set aframe-vr-janus-rooms $package_id $janus_room
     }
 
     return $janus_room
