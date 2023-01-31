defaa8b500146d413148de14e9af2fa9208bf74e
Antonio Pisano <antonio@elettrotecnica.it>
2023-01-31 08:34:38 +0100

    Handle Janus room creation errors gracefully

--- tcl/aframe-vr-init.tcl
+++ tcl/aframe-vr-init.tcl
@@ -9,8 +9,12 @@ ns_register_proc GET /aframe-vr/connect ::ws::aframevr::connect
 nsv_set aframe-vr-janus-rooms mutex [ns_mutex create]
 
 # Create the janus rooms for every package instance
-foreach package_id [apm_package_ids_from_key -package_key aframe-vr -mounted] {
-    aframe_vr::room::require -package_id $package_id
+try {
+    foreach package_id [apm_package_ids_from_key -package_key aframe-vr -mounted] {
+        aframe_vr::room::require -package_id $package_id
+    }
+} on error {errmg} {
+    ad_log warning "Could not create the Janus rooms for VR. Is the Janus server down?" $errmsg
 }
 
 
