a36d86a6c823c07fa7deb68db4a86a031d01a33e
Antonio Pisano <antonio@elettrotecnica.it>
2023-11-12 23:14:50 +0100

    Provide a more generic api to read the environment manifest

--- tcl/environment-procs.tcl
+++ tcl/environment-procs.tcl
@@ -7,33 +7,38 @@ ad_library {
 namespace eval aframe_vr {}
 namespace eval aframe_vr::environment {}
 
-ad_proc -private aframe_vr::environment::get_streaming_surfaces {
+ad_proc -private aframe_vr::environment::read_manifest {
     environment
 } {
-    @return a list of streaming surfaces advertised by the
-            environment's manifest file.
+    @return the manifest data as dict
 } {
-    set environment_manifest_file [acs_package_root_dir [ad_conn package_key]]/environments/${environment}/manifest.json
+    set environment_manifest_file [acs_package_root_dir aframe-vr]/environments/${environment}/manifest.json
 
     if {[ad_file exists $environment_manifest_file]} {
         if {[namespace which ::json::json2dict] eq ""} {
-            package require json
-        }
+	    package require json
+	}
 
         set rfd [open $environment_manifest_file r]
         set json [read $rfd]
         close $rfd
 
         try {
-            set manifest [::json::json2dict $json]
-            set surfaces [dict get $manifest surfaces]
+            return [::json::json2dict $json]
         } on error {errmsg} {
             ad_log error "Invalid JSON manifest\n\n$json\n\n$errmsg"
-            set surfaces [list]
         }
-    } else {
-        set surfaces [list]
     }
 
-    return $surfaces
+    return {}
+}
+
+ad_proc -private aframe_vr::environment::get_streaming_surfaces {
+    environment
+} {
+    @return a list of streaming surfaces advertised by the
+            environment's manifest file.
+} {
+    set manifest [aframe_vr::environment::read_manifest]
+    return [dict get $manifest surfaces]
 }
